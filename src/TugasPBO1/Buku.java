6e4287485a37def4937fec3ceeb0775de7fe34d8
DWira23 <55954299+DWira23@users.noreply.github.com>
2019-10-01 09:54:30 +0700

    Update Buku.java

--- src/TugasPBO1/Buku.java
+++ src/TugasPBO1/Buku.java
@@ -12,6 +12,10 @@ public class Buku {
     public void setAuthor(String Author) {
         this.Author = Author;
     }
+    
+    public void setName(String Author){
+        this.Name = Name;
+    }
 
     public void setYear(int Year) {
         this.Year = Year;
@@ -28,6 +32,10 @@ public class Buku {
     public String getAuthor() {
         return Author;
     }
+    
+    public String getName(){
+        return Name;
+    }
 
     public int getYear() {
         return Year;
@@ -41,8 +49,9 @@ public class Buku {
         return Price;
     }
 
-    Buku(String Author, int Year, int Print, double Price) {
+    Buku(String Author, String Name, int Year, int Print, double Price) {
         this.Author = Author;
+        this.Name = Name;
         this.Year = Year;
         this.Print = Print;
         this.Price = Price;
@@ -52,6 +61,7 @@ public class Buku {
         System.out.println("-------------------------------------------------");
         System.out.println("--------======== BOOK INFORMATION ========--------");
         System.out.println("| Author\t\t: " + this.getAuthor());
+        System.out.println("| Title Book\t\t: " + this.getName());
         System.out.println("| Year of Publication\t: " + this.getYear());
         System.out.println("| Number of Print\t: " + this.getPrint());
         System.out.println("| Price\t\t\t: Rp." + this.getPrice());
